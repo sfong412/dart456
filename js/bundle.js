f89b278c8e044398189850e7afda2de282c64cc9
Sherman <sfong.ima@gmail.com>
2020-04-08 00:29:21 -0400

    added animation to index html and changed bio

---
+++ js/bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module 'popper.js' from 'C:\\Users\\sherm_i7qlnpb\\Documents\\Concord\\dart498\\dart456\\node_modules\\bootstrap\\dist\\js'");
--- js/script.js
+++ js/script.js
@@ -1,3 +1,57 @@
-import Swup from 'swup';
+var anime = require('animejs');
+var bootstrap = require('bootstrap');
 
-const swup = new Swup(); // only this line when included with script tag
+var bannerText = document.querySelector('.home-banner-text');
+
+var textArray = [
+  "sherm is a guy",
+  "sherm is a web developer",
+  "sherm is a sound designer",
+  "sherm is a music composer",
+  "sherm is a digital artist",
+  "sherm is a programmer",
+  "sherm is a nickname"
+];
+
+var textIndex = Math.floor(Math.random() * textArray.length);
+
+var animReset = anime.set('.home_banner .home-banner-text', {
+  opacity: 0
+});
+
+anime({
+  targets: '.home_banner .home-banner-text',
+  keyframes: [
+    {
+      translateY: '-100px',
+      duration: 0,
+      opacity: 0,
+    },
+    {
+      translateY: '0px',
+      duration: 2500,
+      opacity: 100,
+    },
+    {
+      translateY: '50px',
+      duration: 2500,
+      opacity: -100,
+    },
+    {
+      translateY: '-100px',
+      duration: 0,
+      opacity: 0,
+    },
+  ],
+  easing: 'easeOutElastic(1, .5)',
+  loop: true,
+
+  update: function () {
+    bannerText.innerHTML = textArray[textIndex].toString();
+  },
+
+  loopComplete: function() {
+    textIndex = Math.floor(Math.random() * textArray.length);
+    console.log('loop ended');
+  }
+});
